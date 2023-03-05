f06961de327fbb24729b07c0dbf522f298c4385b
maijun <maijun_job@163.com>
2023-03-05 22:21:09 +0800

    type inference

---
+++ javaparser-demo/src/main/java/sec/maijun/javaparser/demo/typesolver/BasicTypeSolverExample.java
@@ -0,0 +1,76 @@
+package sec.maijun.javaparser.demo.typesolver;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import sec.maijun.javaparser.demo.typesolver.util.SolverUtil;
+
+import java.io.IOException;
+
+/**
+ * 本例简单给出了 JavaParser 中，如何对 变量、表达式、函数调用 等进行类型推断
+ *
+ * @since 2023-03-05
+ * @author maijun
+ */
+public class BasicTypeSolverExample {
+    public static void main(String[] args) throws IOException {
+        testTypeInference();
+    }
+
+    public static void testTypeInference() throws IOException {
+        var cu = genCUWithTypeSolverConfig();
+
+        System.out.println("==========> variable or expression type inference");
+        cu.findAll(Expression.class).forEach(expression -> {
+            var resolvedType = expression.calculateResolvedType();
+            if (resolvedType.isPrimitive()) {
+                var typeName = resolvedType.asPrimitive().describe();
+                System.out.println("type for \"" + expression + "\" is " + typeName);
+            } else {
+                System.out.println("type for \"" + expression + "\" is " + expression.calculateResolvedType());
+            }
+        });
+
+        System.out.println("==========> function type inference");
+        cu.findAll(MethodCallExpr.class).forEach(methodCallExpr -> {
+            var reflectionMethodDeclaration = methodCallExpr.resolve();
+            if (reflectionMethodDeclaration != null) {
+                // 这里还可以获得很多内容，比如 package，class 等，可以直接基于这些内容和其他的AST节点联合查询
+                var resolvedQualifiedSignature = reflectionMethodDeclaration.getQualifiedSignature();
+                System.out.println("type for \"" + methodCallExpr + "\" is " + resolvedQualifiedSignature);
+            }
+        });
+    }
+
+
+    private static CompilationUnit genCUWithTypeSolverConfig() throws IOException {
+        var content = """
+                import java.util.List;
+                import java.util.ArrayList;
+                
+                public class Hello {
+                    public static void main(String[] args) {
+                        int x = 3;
+                        int y = 4;
+                        int z = x + y;
+                        int z1 = z * x;
+                        z1 *= y;
+                        System.out.println(z1);
+                    }
+                }
+                """;
+
+        // 当前我们简单解析单文件，还不涉及跨文件或者第三方依赖
+        var typeSolver = SolverUtil.generateTypeSolver(null, null);
+        var symbolSolver = new JavaSymbolSolver(typeSolver);
+        var configuration = new ParserConfiguration();
+        configuration.setSymbolResolver(symbolSolver);
+        var parser = new JavaParser(configuration);
+        var result = parser.parse(content);
+        return result.getResult().get();
+    }
+}
---
+++ javaparser-demo/src/main/java/sec/maijun/javaparser/demo/typesolver/util/SolverUtil.java
@@ -0,0 +1,57 @@
+package sec.maijun.javaparser.demo.typesolver.util;
+
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 关于 TypeSolver 的解释
+ *
+ * 1) AarTypeSolver: 在Android aar文件中查找特定的类型;
+ * 2) ClassLoaderTypeSolver: 针对自定义的ClassLoader使用，可以处理由该ClassLoader加载的类，这种类型在静态代码分析中很少用到;
+ * 3) CombinedTypeSolver: 可以封装多个Type Solver一起使用
+ * 4) JarTypeSolver: 在Jar文件中，查找特定的类型
+ * 5) JavaParserTypeSolver: 在源码中查找特定的类型，只需要传递源码根路径即可
+ * 6) MemoryTypeSolver: 一般不需要使用，可以在测试中使用
+ * 7) ReflectionTypeSolver: 用来处理应用classpath里面的类，一般用于处理JRE中的类
+ *
+ * 通常，作为一个解析 Java 代码的工具，一般解析来源有三个：
+ * 1) 当前模块中的其他源码；
+ * 2) 其他模块中的 class 文件；
+ * 3) 依赖的 jar 中的信息
+ */
+public class SolverUtil {
+
+    /**
+     * 需要处理的类型有三个来源：① 当前自己工程中定义；② 当前工程的第三方依赖；③ JRE中的基本类型。
+     * 对应上面的JavaParserTypeSolver，JarTypeSolver和ReflectionTypeSolver，组合成一个CombinedTypeSolver
+     *
+     * @param sourcePath
+     * @param dependencies
+     * @return
+     * @throws IOException
+     */
+    public static CombinedTypeSolver generateTypeSolver(String sourcePath, List<String> dependencies) throws IOException {
+        CombinedTypeSolver solver = new CombinedTypeSolver();
+        // 1. JavaParserTypeSolver
+        if (sourcePath != null) {
+            solver.add(new JavaParserTypeSolver(sourcePath));
+        }
+
+        // 2. JarTypeSolver
+        if (dependencies != null) {
+            for (String dependency : dependencies) {
+                solver.add(new JarTypeSolver(dependency));
+            }
+        }
+
+        // 3. ReflectionTypeSolver
+        solver.add(new ReflectionTypeSolver());
+
+        return solver;
+    }
+}
